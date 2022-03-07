674637d7acb6fcfb4e0f479b1f9ca64a84cf1d0f
Ruslan Yakovenko <ruslan.iakovenko@nure.ua>
2022-03-08 00:00:25 +0200

    Solved 35

---
+++ leetcode/35. Search Insert Position/src/org/training/Main.java
@@ -0,0 +1,33 @@
+package org.training;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return right;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
