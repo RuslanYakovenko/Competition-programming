e388ade22b257e36d9abfe002038993cda2e30e2
Ruslan Yakovenko <ruslan.iakovenko@nure.ua>
2022-05-04 15:24:20 +0300

    Leetcode 44

--- leetcode/44.WildcardMatching/src/org/training/Main.java
+++ leetcode/44.WildcardMatching/src/org/training/Main.java
@@ -1,55 +1,24 @@
 package org.training;
 
 class Solution {
-    public boolean isMatch(String s, String p) {
-        boolean isUniversal = false;
-        int index = 0;
-        int indexInString = 0;
-        int start = -1;
-        int endInPattern = 0;
-        boolean answer = false;
-        while (index < p.length()) {
-            if (indexInString >= s.length()) {
-                return false;
-            }
-            if (p.charAt(index) == '*') {
-                endInPattern = index;
-                start = indexInString;
-                index++;
-                isUniversal = true;
-                answer = true;
-            } else if (p.charAt(index) == '?') {
-                answer = true;
-                indexInString++;
-                index++;
-            } else {
-                if (isUniversal) {
-                    if (p.charAt(index) == s.charAt(indexInString)) {
-                        start = start == -1 ?  indexInString : start;
-                        index++;
-                        indexInString++;
-                        answer = true;
-                    } else {
-                        answer = false;
-                        index = endInPattern;
-                        indexInString = ++start;
-
-                    }
+    public boolean isMatch(String str, String pattern) {
+        boolean[][] dp = new boolean[pattern.length() + 1][str.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < pattern.length() + 1; i++) {
+            if (pattern.charAt(i - 1) == '*')
+                dp[i][0] = dp[i - 1][0];
+        }
+        for (int i = 1; i < pattern.length() + 1; i++) {
+            for (int j = 1; j < str.length() + 1; j++) {
+                if (pattern.charAt(i - 1) != '*') {
+                    if (pattern.charAt(i - 1) == str.charAt(j - 1) || pattern.charAt(i - 1) == '?')
+                        dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    if (p.charAt(index) == s.charAt(indexInString)) {
-                        start = start == -1 ? indexInString : start;
-                        answer = true;
-                        index++;
-                        indexInString++;
-                    } else {
-                        answer = false;
-                        index = endInPattern;
-                        indexInString = ++start;
-                    }
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                 }
             }
         }
-        return answer;
+        return dp[pattern.length()][str.length()];
     }
 }
 
@@ -57,8 +26,9 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "cb";
-        String p = "******";
-        System.out.println(solution.isMatch(s, p));
+        String s = "qwqeefgzdqwefgzqeqwegwh3232323";
+        String p = "*fgz??qwe*gwh*";
+        boolean result = solution.isMatch(s, p);
+        System.out.println(result);
     }
 }
